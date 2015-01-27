f6376c2c02b04816a3fdd52d44742dd7a7c5bd46
Alexander Nilsson <alexander@alxoptr.se>
2015-01-27 15:19:06 +0100

    Initial commit

---
+++ gulpfile.js
@@ -0,0 +1,75 @@
+var gulp         = require('gulp');
+var sass         = require('gulp-sass');
+var	autoprefixer = require('gulp-autoprefixer');
+var	browserSync  = require('browser-sync');
+var	reload       = browserSync.reload;
+var taskListing  = require('gulp-task-listing');
+
+var config = {
+	sass: {
+		base: 'app/assets/sass/*.scss',
+		compiled: 'public/css/',
+		outputStyle: 'nested',
+	},
+
+	bs: {
+		proxy: '192.168.1.97:8000',
+		browsers: ['google chrome', 'firefox', 'safari']
+	},
+	
+	blade: '**/*.blade.php',
+	js: 'public/js/**/*.js',
+	watchers: ['watch-blade', 'watch-css', 'watch-js', 'watch-sass'],
+	compilers: ['compile-sass'],
+}
+
+// List tasks as default task
+gulp.task('default', taskListing);
+
+// Watchers
+gulp.task('watch', config.watchers, function(){
+	browserSync({
+		proxy: config.bs.proxy,
+		browser: config.bs.browsers,
+		ghostMode: {
+			clicks: true,
+			location: true,
+			forms: true,
+			scroll: true
+		}
+	});
+});
+gulp.task('watch-blade', function(){
+	gulp.watch(config.blade, ['reload']);
+});
+gulp.task('watch-css', function(){
+	gulp.watch(config.sass.compiled + '*.css', function(){
+		gulp.src(config.sass.compiled + '*.css')
+			.pipe(reload({ stream: true }));
+	});
+});
+gulp.task('watch-js', function(){
+	gulp.watch(config.js, function(){
+		gulp.src(config.js)
+			.pipe(reload({ stream: true }));
+	});
+});
+gulp.task('watch-sass', function(){
+	gulp.watch(config.sass.base, ['compile-sass']);
+});
+
+// Compilers
+gulp.task('compile', config.compilers);
+gulp.task('compile-sass', function(){
+	gulp.src(config.sass.base)
+		.pipe(sass({ errLogToConsole: true, outputStyle: config.sass.outputStyle }))
+		.pipe(autoprefixer('last 2 versions', '> 1%', 'ff 17', 'opera 12.1'))
+		.pipe(gulp.dest(config.sass.compiled));
+});
+
+// Reloaders
+// Easy task to send a browsersync livereload
+gulp.task('reload', function(){
+	gulp.src('.')
+		.pipe(reload({ stream: true }));
+});
