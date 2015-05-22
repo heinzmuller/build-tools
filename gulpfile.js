aad34be994f155d88ed377920a83b2c4ba1f176b
heinzmuller <alexander.nilsson@seedit.org>
2015-05-23 00:34:16 +0200

    Update gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -1,27 +1,28 @@
 var gulp         = require('gulp');
 var sass         = require('gulp-sass');
-var autoprefixer = require('gulp-autoprefixer');
-var browserSync  = require('browser-sync');
-var reload       = browserSync.reload;
+var	autoprefixer = require('gulp-autoprefixer');
+var	browserSync  = require('browser-sync');
+var	reload       = browserSync.reload;
 var taskListing  = require('gulp-task-listing');
+var sourcemaps   = require('gulp-sourcemaps');
 var publish      = require('./publish.json');
 
 var config = {
-  sass: {
-    base: 'app/assets/sass/*.scss',
-    compiled: 'public/css/',
-    outputStyle: 'nested',
-  },
+	sass: {
+		base: 'app/assets/sass/*.scss',
+		compiled: 'public/css/',
+		outputStyle: 'nested',
+	},
 
-  bs: {
-    proxy: 'localhost:8000',
-    browsers: ['google chrome', 'firefox', 'safari']
-  },
-  
-  blade: '**/*.blade.php',
-  js: 'public/js/**/*.js',
-  watchers: ['watch-blade', 'watch-css', 'watch-js', 'watch-sass'],
-  compilers: ['compile-sass'],
+	bs: {
+		proxy: 'localhost:8000',
+		browsers: ['google chrome'] // firefox, safari
+	},
+	
+	blade: '**/*.blade.php',
+	js: 'public/js/**/*.js',
+	watchers: ['compile-sass', 'watch-blade', 'watch-css', 'watch-js', 'watch-sass'],
+	compilers: ['compile-sass'],
 }
 
 // List tasks as default task
@@ -29,53 +30,54 @@ gulp.task('default', taskListing);
 
 // Watchers
 gulp.task('watch', config.watchers, function(){
-  browserSync({
-    proxy: config.bs.proxy,
-    browser: config.bs.browsers,
-    ghostMode: {
-      clicks: true,
-      location: true,
-      forms: true,
-      scroll: true
-    }
-  });
+	browserSync({
+		proxy: config.bs.proxy,
+		browser: config.bs.browsers,
+		ghostMode: {
+			clicks: true,
+			location: true,
+			forms: true,
+			scroll: true
+		}
+	});
 });
 gulp.task('watch-blade', function(){
-  gulp.watch(config.blade, ['reload']);
+	gulp.watch(config.blade, ['reload']);
 });
 gulp.task('watch-css', function(){
-  gulp.watch(config.sass.compiled + '*.css', function(){
-    gulp.src(config.sass.compiled + '*.css')
-    .pipe(reload({ stream: true }));
-  });
+	gulp.watch(config.sass.compiled + '*.css', function(){
+		gulp.src(config.sass.compiled + '*.css')
+			.pipe(reload({ stream: true }));
+	});
 });
 gulp.task('watch-js', function(){
-  gulp.watch(config.js, function(){
-    gulp.src(config.js)
-    .pipe(reload({ stream: true }));
-  });
+	gulp.watch(config.js, function(){
+		gulp.src(config.js)
+			.pipe(reload({ stream: true }));
+	});
 });
 gulp.task('watch-sass', function(){
-  gulp.watch(config.sass.base, ['compile-sass']);
+	gulp.watch(config.sass.base, ['compile-sass']);
 });
 
 // Compilers
 gulp.task('compile', config.compilers);
 gulp.task('compile-sass', function(){
-  gulp.src(config.sass.base)
-  .pipe(sass({ errLogToConsole: true, outputStyle: config.sass.outputStyle }))
-  .pipe(autoprefixer('last 2 versions', '> 1%', 'ff 17', 'opera 12.1'))
-  .pipe(gulp.dest(config.sass.compiled));
+	gulp.src(config.sass.base)
+		.pipe(sourcemaps.init())
+		.pipe(sass({ errLogToConsole: true, outputStyle: config.sass.outputStyle }))
+		.pipe(autoprefixer('last 2 versions', '> 1%', 'ff 17', 'opera 12.1'))
+		.pipe(sourcemaps.write())
+		.pipe(gulp.dest(config.sass.compiled));
 });
 
 // Reloaders
 // Easy task to send a browsersync livereload
 gulp.task('reload', function(){
-  gulp.src('.')
-  .pipe(reload({ stream: true }));
+	gulp.src('.')
+		.pipe(reload({ stream: true }));
 });
 
-// Publish files defined in publish.json file 
 gulp.task('publish', function(){
   publish.files.forEach(function(file){
     gulp.src(file.src)
